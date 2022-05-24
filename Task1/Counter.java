2c8f965c48828dbec82ace689a18a7364ca6885c
PC <a.bia@campus.fct.unl.pt>
2022-05-24 14:11:45 +0100

    Counter class done

--- Task1/Counter.java
+++ Task1/Counter.java
@@ -6,13 +6,52 @@ public class Counter {
     private int limit;
     private boolean overflow;
 
-    public Counter(int val, int limit) {  }
+    /*@
+    predicate CounterInv(int v, int lim, boolean over) = this.val |-> v &*& this.limit |-> lim &*& this.overflow |-> over &*& 0 <= v &*& v < lim;
+    @*/
 
-    public int getVal() { return 0; }
+    public Counter(int val, int limit)
+    //@ requires 0 <= val &*& val < limit &*& limit > 0;
+    //@ ensures CounterInv(val, limit, false);
+    {
+        this.val = val;
+        this.limit = limit;
+        this.overflow = false;
+    }
 
-    public int getLimit() { return 0; }
+    public int getVal()
+    //@ requires CounterInv(?value, ?lim, ?over);
+    //@ ensures CounterInv(value, lim, over) &*& result == value;
+    { 
+        return val; 
+    }
 
-    public void incr(int v) {  }
+    public int getLimit()
+    //@ requires CounterInv(?value, ?lim, ?over);
+    //@ ensures CounterInv(value, lim, over) &*& result == lim;
+    { 
+        return limit; 
+    }
+
+    public void incr(int v)
+    //@ requires CounterInv(?value, ?lim, ?over) &*& v >= 0;
+    //@ ensures ((value + v) >= lim) ? (CounterInv((value + v) % lim, lim, true)) : CounterInv(value + v, lim, over);
+    { 
+        if(val + v >= limit){
+            val = (val + v) % limit;  
+            overflow = true;
+        } else
+            val = val + v;
+     }
     
-    public void decr(int v) {  }
+    public void decr(int v)
+    //@ requires CounterInv(?value, ?lim, ?over) &*& v >= 0;
+    //@ ensures (value - v < 0) ? (CounterInv(0, lim, true)) : CounterInv(value - v, lim, over); 
+    { 
+        if(val - v < 0) {
+            val = 0;
+            overflow = true;
+        } else
+            val = val - v;
+     }
 }
